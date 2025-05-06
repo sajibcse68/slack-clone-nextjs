6411bac7af3fbd959037265edc23d07869af05f8
sajibcse68 <sajibcse68@gmail.com>
2025-05-07 01:47:10 +0600

    Implement api to get user details, render dropdown menu with user details & log out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { useAuthActions } from "@convex-dev/auth/react";
-import { Button } from "@/components/ui/button";
+import { UserButton } from '@/features/auth/components/user-button';
 
 export default function Home() {
-  const { signOut } = useAuthActions();
 
-  return <div> Logged In!
-
-    <Button onClick={() => signOut()}>Sign Out</Button>
-  </div>;
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
 }
