aaf4752863c2756c94d8f1a507d84cdea6735d7e
sajibcse68 <sajibcse68@gmail.com>
2025-07-10 05:18:10 +0600

    Implement workspace resizable left sidebar, Header, Dropdown items, Tooltip

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,14 @@
 'use client';
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
+
 import Sidebar from './sidebar';
 import { Toolbar } from './toolbar';
+import WorkspaceSidebar from './workspace-sidebar';
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
@@ -12,9 +19,26 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
     <div className="h-full">
       <Toolbar />
 
-      <div className='flex h-[calc(100vh-40px)]'>
+      <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}</div>
+
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="sa-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+
+          <ResizableHandle withHandle />
+
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </div>
   );
 };
